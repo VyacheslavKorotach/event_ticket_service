2a0a64c1996207cd5c34dc709684d47151c6b074
Vyacheslav Korotach <vkorotach@gmail.com>
2021-03-29 11:59:22 +0300

    EventFactory changed

--- contracts/eventfactory.sol
+++ contracts/eventfactory.sol
@@ -5,7 +5,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EventFactory is ERC721, Ownable {
-    constructor() ERC721("eventNFT", "ENFT") {
-    }
+contract EventFactory is Ownable {
+    ERC721[] public events;
+
+    mapping (uint => address) public eventToOwner;
+    mapping (address => uint) ownerEventCount;
+
 }
