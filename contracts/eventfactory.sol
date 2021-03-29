e3a60a090919664d1d47aef929d9b7d69bccaac9
Vyacheslav Korotach <vkorotach@gmail.com>
2021-03-29 12:44:22 +0300

    _createEvent added

--- contracts/eventfactory.sol
+++ contracts/eventfactory.sol
@@ -6,9 +6,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EventFactory is Ownable {
+    event NewEvent(uint eventId, string name, string symbol);
+
     ERC721[] public events;
 
     mapping (uint => address) public eventToOwner;
     mapping (address => uint) ownerEventCount;
 
+    function _createEvent(string memory name_, string memory symbol_) internal {
+        events.push(new ERC721(name_, symbol_));
+        uint id = events.length;
+        eventToOwner[id] = msg.sender;
+        ownerEventCount[msg.sender] = ownerEventCount[msg.sender]++;
+        emit NewEvent(id, name_, symbol_);
+    }
 }
