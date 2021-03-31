87e573d9772b891c805d903a3a5fb44625aeb905
Vyacheslav Korotach <vkorotach@gmail.com>
2021-03-31 22:19:31 +0300

    test/helpers/utils.js added

--- test/TicketOffice.js
+++ test/TicketOffice.js
@@ -1,4 +1,5 @@
 const TicketOffice = artifacts.require("TicketOffice");
+const utils = require("./helpers/utils");
 const eventNames = ["Event 1", "Event 2"];
 const eventSymbols = ["EVN1", "EVN2"];
 const eventTotalTickets = ["100", "200"];
@@ -6,20 +7,23 @@ const eventTicketPrices = ["10", "20"];
 const eventDescriptions = ["desription 1", "description 2"];
 const eventLocations = ["location 1", "location 2"];
 const eventStartDates = ["start Date 1", "start Date 2"];
-const eventEmdDates = ["end Date 1", "end Date 2"];
+const eventEndDates = ["end Date 1", "end Date 2"];
 contract("TicketOffice", (accounts) => {
     let [alice, bob] = accounts;
-
-    // start here
+    let contractInstance;
+    beforeEach(async () => {
+        contractInstance = await TicketOffice.new();
+    });
 
     it("should be able to create a new event", async () => {
-        const contractInstance = await TicketOffice.new();
         const result = await contractInstance.createNewEvent(
             eventNames[0], eventSymbols[0], eventTotalTickets[0], eventTicketPrices[0], eventDescriptions[0],
-            eventLocations[0], eventStartDates[0], eventEmdDates[0], {from: alice}
+            eventLocations[0], eventStartDates[0], eventEndDates[0], {from: alice}
         );
         assert.equal(result.receipt.status, true);
-        // assert.equal(result.logs[0].args.name, eventNames[0]);
+        assert.equal(result.logs[0].args.name, eventNames[0]);
+        assert.equal(result.logs[0].args.symbol, eventSymbols[0]);
+        // console.log(result.logs[0]);
     })
 
     //define the new it() function
