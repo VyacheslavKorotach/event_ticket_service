5cc89ce984eaae415f176dda28570b0e134f53bd
Vyacheslav Korotach <vkorotach@gmail.com>
2021-04-01 22:03:55 +0300

    Three first tests passed

--- test/TicketOffice.js
+++ test/TicketOffice.js
@@ -1,9 +1,8 @@
 const TicketOffice = artifacts.require("TicketOffice");
 // const utils = require("./helpers/utils");
 const eventNames = ["Event 1", "Event 2"];
-const eventSymbols = ["EVN1", "EVN2"];
 const eventTotalTickets = ["100", "200"];
-const eventTicketPrices = ["10", "20"];
+const eventTicketPrices = ["10000000000000000", "20000000000000000"];
 const eventDescriptions = ["desription 1", "description 2"];
 const eventLocations = ["location 1", "location 2"];
 const eventStartDates = ["start Date 1", "start Date 2"];
@@ -17,31 +16,18 @@ contract("TicketOffice", (accounts) => {
 
     it("should be able to create a new event", async () => {
         const result = await contractInstance.createNewEvent(
-            eventNames[0], eventSymbols[0], eventTotalTickets[0], eventTicketPrices[0], eventDescriptions[0],
-            eventLocations[0], eventStartDates[0], eventEndDates[0], {from: alice}
+            eventNames[0], eventDescriptions[0], eventLocations[0], eventStartDates[0],
+            eventEndDates[0], eventTicketPrices[0], eventTotalTickets[0], {from: alice}
         );
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[0].args.name, eventNames[0]);
-        assert.equal(result.logs[0].args.symbol, eventSymbols[0]);
-        // console.log(result);
-    })
-
-    it("should be able to create a new ticket", async () => {
-        const result = await contractInstance.createNewEvent(
-            eventNames[0], eventSymbols[0], eventTotalTickets[0], eventTicketPrices[0], eventDescriptions[0],
-            eventLocations[0], eventStartDates[0], eventEndDates[0], {from: alice}
-        );
-        // const eventId = result.logs[0].args.eventId.toNumber();
-        assert.equal(result.receipt.status, true);
-        assert.equal(result.logs[0].args.name, eventNames[0]);
-        assert.equal(result.logs[0].args.symbol, eventSymbols[0]);
         // console.log(result.logs[0].args.eventId);
     })
 
     it("should be able to get the event details", async () => {
         const result = await contractInstance.createNewEvent(
-            eventNames[0], eventSymbols[0], eventTotalTickets[0], eventTicketPrices[0], eventDescriptions[0],
-            eventLocations[0], eventStartDates[0], eventEndDates[0], {from: alice}
+            eventNames[0], eventDescriptions[0], eventLocations[0], eventStartDates[0],
+            eventEndDates[0], eventTicketPrices[0], eventTotalTickets[0], {from: alice}
         );
         const result2 = await contractInstance.getEventDetails("1", {from: alice} );
         assert.equal(result.receipt.status, true);
@@ -50,16 +36,12 @@ contract("TicketOffice", (accounts) => {
 
     it("should be able to buy the ticket", async () => {
         const result = await contractInstance.createNewEvent(
-            eventNames[0], eventSymbols[0], eventTotalTickets[0], eventTicketPrices[0], eventDescriptions[0],
-            eventLocations[0], eventStartDates[0], eventEndDates[0], {from: alice}
+            eventNames[0], eventDescriptions[0], eventLocations[0], eventStartDates[0],
+            eventEndDates[0], eventTicketPrices[0], eventTotalTickets[0], {from: alice}
         );
-        const result3 = await contractInstance.buyTicket("1", {from: bob} )
-        // assert.equal(result.logs[0].args.name, eventNames[0]);
-        // assert.equal(result.logs[0].args.symbol, eventSymbols[0]);
-        console.log(result3);
+        const result3 = await contractInstance.buyTicket("1", {from: bob, value: "10000000000000000"} )
         assert.equal(result.receipt.status, true);
-        // assert.equal(result.logs[0].args.name, eventNames[0]);
-        // assert.equal(result.logs[0].args.symbol, eventSymbols[0]);
+        assert.equal(result3.receipt.status, true);
     })
 
 })
