90312e6f3692892f51524c8ce79aa30b90b7de8d
Vyacheslav Korotach <vkorotach@gmail.com>
2021-04-18 15:11:22 +0300

    constructor of ERC721 changed

--- contracts/UserEvent.sol
+++ contracts/UserEvent.sol
@@ -23,7 +23,7 @@ contract UserEvent is ERC721 {
 
     eventDetails[] public events;
 
-    constructor() ERC721("Event Ticket Service", "ETC") {
+    constructor() ERC721("EventTicketService", "ETC") {
         totalEventCount = 0;
         activeEventCount = 0;
         totalTicketCount = 0;
